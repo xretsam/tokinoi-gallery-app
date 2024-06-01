dae656cd19becc13a3c1145d32705335cd3b791c
Adilkhan Yerbolat <dilhanmq@gmail.com>
2024-06-01 22:43:05 +0500

    Initial commit!

---
+++ backend/src/main/java/ru/meinone/tokinoi_gallery_app/TokinoiGalleryAppApplication.java
@@ -0,0 +1,13 @@
+package ru.meinone.tokinoi_gallery_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokinoiGalleryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokinoiGalleryAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/meinone/tokinoi_gallery_app/TokinoiGalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.meinone.tokinoi_gallery_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokinoiGalleryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
