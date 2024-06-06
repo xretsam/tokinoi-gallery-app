3c1931510e7179cce3ff599ebcdddac07d3aede9
Adilkhan Yerbolat <dilhanmq@gmail.com>
2024-06-06 16:55:55 +0500

    implemented API for images

--- backend/src/main/java/ru/meinone/tokinoi_gallery_app/controller/ImageController.java
+++ backend/src/main/java/ru/meinone/tokinoi_gallery_app/controller/ImageController.java
@@ -1,6 +1,10 @@
 package ru.meinone.tokinoi_gallery_app.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.meinone.tokinoi_gallery_app.service.ImageService;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/image")
@@ -22,6 +29,19 @@ public class ImageController {
             return ResponseEntity.ok("Image uploaded successfully");
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save image");
+        }
+    }
+    @GetMapping("/{imageUrl:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable("imageUrl") String imageUrl) {
+        try {
+            Resource resource = imageService.getImage(imageUrl);
+            String contentType = imageService.getContentType(imageUrl);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .header(HttpHeaders.CONTENT_TYPE, contentType)
+                    .body(resource);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 }
--- backend/src/main/java/ru/meinone/tokinoi_gallery_app/service/ImageService.java
+++ backend/src/main/java/ru/meinone/tokinoi_gallery_app/service/ImageService.java
@@ -2,6 +2,8 @@ package ru.meinone.tokinoi_gallery_app.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.meinone.tokinoi_gallery_app.model.Image;
@@ -27,7 +29,7 @@ public class ImageService {
             Files.createDirectories(uploadPath);
         }
         long creationTime = System.currentTimeMillis();
-        String fileName = galleryId + "_" + creationTime;
+        String fileName = galleryId + "_" + creationTime + "." + getFileExtension(file) ;
         Path imagePath = uploadPath.resolve(fileName);
         Files.write(imagePath, file.getBytes());
 
@@ -36,5 +38,25 @@ public class ImageService {
         image.setUrl(fileName);
         image.setCreatedAt(new Date(creationTime));
         imageRepository.save(image);
+    }
+    public Resource getImage(String imageUrl) throws IOException {
+        Path imagePath = Paths.get(uploadDir, imageUrl).normalize();
+        Resource resource = new UrlResource(imagePath.toUri());
+        if (resource.exists() && resource.isReadable()) {
+            return resource;
+        } else {
+            throw new IOException("Could not read image: " + imageUrl);
+        }
+    }
+    public String getContentType(String imageUrl) throws IOException {
+        Path imagePath = Paths.get(uploadDir, imageUrl).normalize();
+        return Files.probeContentType(imagePath);
+    }
+    private String getFileExtension(MultipartFile file) {
+        String originalFilename = file.getOriginalFilename();
+        if (originalFilename == null || originalFilename.lastIndexOf(".") == -1) {
+            return null;
+        }
+        return originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
     }
 }
