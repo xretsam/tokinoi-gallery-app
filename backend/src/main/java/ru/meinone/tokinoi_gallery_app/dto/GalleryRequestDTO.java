1dcd59ac45530a06fe2201cf1cbb455732e8b5d6
Yerbolat Adilkhan <dilhanmq@gmail.com>
2024-07-15 11:28:55 +0500

    feat: add image validation to galleryRequestDTO

--- backend/src/main/java/ru/meinone/tokinoi_gallery_app/dto/GalleryRequestDTO.java
+++ backend/src/main/java/ru/meinone/tokinoi_gallery_app/dto/GalleryRequestDTO.java
@@ -4,12 +4,13 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 import ru.meinone.tokinoi_gallery_app.model.Category;
+import ru.meinone.tokinoi_gallery_app.util.validation.constraint.ImageContentConstraint;
 
 import java.util.List;
 
 @Data
 public class GalleryRequestDTO {
-
+    @ImageContentConstraint
     private MultipartFile thumbnail;
 
     @Size(max = 255, message = "Title cannot be longer than 255 characters")
